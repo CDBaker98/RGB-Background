5d63c528ac828ae47c47f397c23bcf6e83fbd743
Caleb Baker <github@cdbaker98.com>
2023-03-22 13:41:45 -0500

    Added All the Files

---
+++ RGB-Background/BoundProperties.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+
+namespace RGB_Background {
+    public class BoundProperties : INotifyPropertyChanged {
+
+        private bool _RGB;
+        private Brush _Background;
+
+        public bool RGB { get => _RGB; set => SetProperty(ref _RGB, value); }
+        public Brush Background { get => _Background; set => SetProperty(ref _Background, value); }
+
+        public BoundProperties() {
+            RGB = false;
+
+            BrushConverter bc = new();
+            Background = (Brush)bc.ConvertFrom("#ffe0e0e0");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = "") {
+            if (EqualityComparer<T>.Default.Equals(storage, value)) {
+                return;
+            }
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
---
+++ RGB-Background/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace RGB_Background {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        private byte _R = 255;
+        private byte _G, _B = 0;
+        private static BoundProperties BP = new();
+
+        public MainWindow() {
+            InitializeComponent();
+            DataContext = BP;
+            MainWindow_RGB();
+        }
+
+        private async void MainWindow_RGB() {
+            // Cycle through different colors in the 0-255 RGB range
+            if (_R == 255 && _G < 255 && _B == 0) {
+                _G++;
+            }
+            else if (_R > 0 && _G == 255 && _B == 0) {
+                _R--;
+            }
+            else if (_R == 0 && _G == 255 && _B < 255) {
+                _B++;
+            }
+            else if (_R == 0 && _G > 0 && _B == 255) {
+                _G--;
+            }
+            else if (_R < 255 && _G == 0 && _B == 255) {
+                _R++;
+            }
+            else if (_R == 255 && _G == 0 && _B > 0) {
+                _B--;
+            }
+
+
+            if (BP.RGB) {
+                BP.Background = new LinearGradientBrush(Colors.White, Color.FromRgb(_R, _G, _B), 90);
+            }
+            else {
+                var bc = new BrushConverter();
+                BP.Background = (Brush)bc.ConvertFrom("#FFE0E0E0");
+            }
+
+            await Task.Delay(25);
+            MainWindow_RGB();
+        }
+    }
+}
